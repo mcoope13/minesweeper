2f647d64ea87cfedc825819cbbaa6d5e08d57e9c
mcoope13 <mcoope13@gmail.com>
2020-05-11 21:21:04 -0400

    Add files via upload

--- minesweeper.js
+++ minesweeper.js
@@ -1,17 +1,42 @@
-let board = [['','',''], 
-            ['','',''], 
-            ['','','']];
-
-const printBoard = board => {
-   console.log('Current Board:') 
-    console.log(board[0].join('|'));
-    console.log(board[1].join('|'));
-    console.log(board[2].join('|'));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+    for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+        const row = [];
+       for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+            row.push(' ');
+        } 
+        board.push(row);
+    }
+    return board;
 };
 
-printBoard(board);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+        const row = [];
+       for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+            row.push(null);
+        } 
+        board.push(row);
+    } 
+    
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs) {
+        const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+    } 
+    
+    return board; 
+};
 
-board[0][1] = 1;
-board[2][2] = 'B';
+const printBoard = board => { 
+    console.log(board.map(row => row.join(' | ')).join('\n')); 
+};
 
-printBoard(board);
+
+const playerBoard = generatePlayerBoard(4, 3);
+const bombBoard = generateBombBoard(4, 3, 4);
+printBoard(playerBoard);
+printBoard(bombBoard);
